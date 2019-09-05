edcc06318ace9c1f1371f1a6f74a8eaebdf70a40
Madhur Suria Mata Garcia <madhurmata@gmail.com>
2019-09-05 12:16:04 +0200

    refactoring in card component

--- src/app/components/shared/cards/cards.component.ts
+++ src/app/components/shared/cards/cards.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
-  constructor() { }
+  @Input() items: any[] = [];
 
-  ngOnInit() {
-  }
+  constructor() { }
 
 }
